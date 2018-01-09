ccc9ef5337cd1fb6785ba3c0e05363691ff1c474
Celia19C <thatcyclone@gmail.com>
2018-01-09 22:48:48 +0100

    id and details

--- js/main.min.js
+++ js/main.min.js
@@ -1,51 +1,29 @@
 // Menú desplegable
-var realmenu = document.querySelector('.real-menu');
-var button = realmenu.querySelector('.button');
-var menuClose = realmenu.querySelector('.menu-close');
+var realmenu = document.querySelector('.container');
+var button = document.querySelector('.button');
+var menuClose = document.querySelector('.menu-close');
 var option = document.querySelector('.menu');
+var body = document.querySelector('.body');
+var enlaces = document.querySelector('.item');
 
 
 // Functiones para abrir y cerrar
 function openNavMenu() {
-	realmenu.classList.add('realmenu--menu-open')
-realmenu.classList.add('gradient');
+	body.classList.add('menu-open')
 };
 
 function closeNavMenu() {
-	realmenu.classList.remove('realmenu--menu-open');
+	body.classList.remove('menu-open');
+
 };
 
 //Hacer el 'click'
 button.addEventListener('click', openNavMenu);
 menuClose.addEventListener('click', closeNavMenu);
 option.addEventListener('click', closeNavMenu);
-
+// enlaces.addEventListener('click', closeNavMenu);
 
 // AJAX
-// //variable de petición
-// var request= new XMLHttpRequest();
-// var buttonRequest= document.querySelector('.btn-more-reasons');
-//
-// // function MoreReasons() {
-// request.open('GET','https://three-random-reasons-izwfjpgbqm.now.sh/')
-// request.addEventListener('load', addReasons);
-// function addReasons() {
-// var response=request.responseText:
-// var responseJSON= JSON.parse(response).reasons;
-// var listReasons= '';
-// for (var i=0;i<response.length;i++) {
-// listReasons += '<div>'+ responseJSON[i]+ '<div>';
-// }
-// var divReasons= document.querySelector('.div-reasons');
-// div.innerHTML=listReasons;
-//
-// }
-// buttonRequest.addEventListener('click',newRequest);
-// function newRequest() {
-// 	request= new XMLHttpRequest();
-// // moreReasons();
-// }
-// // moreReasons();
 function requestFetch (){
 fetch('https://three-random-reasons-izwfjpgbqm.now.sh')
   .then(function(response){
